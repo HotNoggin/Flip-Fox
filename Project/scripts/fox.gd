4568b48936c59caaeb0e327277b8a0c5c17dd575
Hot Noggin Studios <143744752+HotNoggin@users.noreply.github.com>
2025-01-18 20:42:33 -0600

    Dialogue and cash

--- Project/scripts/fox.gd
+++ Project/scripts/fox.gd
@@ -3,31 +3,36 @@ extends Node3D
 
 @export var bet_label: Label3D
 @export var label: Label3D
+@export var cash_label: Label3D
 @export var coin: Coin
 @export var suspicion: float
 @export var all_calls: Array[bool]
 @export var min_cash: int
 @export var max_cash: int
+@export var player_cash: int = 10
 
 
 var time: float
 var my_call: bool
+var my_bet: int
+var choice: String
 var bet_tween: Tween
 var speech_tween: Tween
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	cash_label.text = "You have $" + str(player_cash) + "."
 	make_call()
 
 
 func make_call() -> void:
 	my_call = randi_range(0, 1) == 1
-	var amount: int = randi_range(2, 5)
-	var choice: String = "tails"
+	my_bet = randi_range(2, 5)
+	choice = "tails"
 	if my_call:
 		choice = "heads"
-	bet_label.text = "I bet $" + str(amount) + " on " + choice
+	bet_label.text = "I bet $" + str(my_bet) + " on " + choice
 	bet_label.scale = Vector3.ZERO
 	if bet_tween:
 		bet_tween.kill()
@@ -38,10 +43,26 @@ func make_call() -> void:
 func check_results() -> void:
 	var result: bool = coin.is_heads
 	all_calls.append(result)
+	
+	var remarks: Array[String]
+	
 	if result == my_call:
-		say("Ha. Called it.")
+		remarks = [ "Called it.", "The coin's on my side.", "Give me your money.",
+			"I'm just lucky.", "I knew it.", "I could just tell.", "I'm a coin whisperer.",
+			"Just like I said.", "Never doubted it.", "You owe me.", "Pay up.",
+			"That's " + str(my_bet) + ", buddy."
+		]
+		player_cash -= my_bet
 	else:
-		say("...")
+		remarks = ["...", "That should've been " + choice + ".", "That's strange.",
+			"That isn't right...", "Why wasn't it " + choice + "?", "Just my luck.",
+			"Bad guess.", "I guess I owe you.", "Fine, take my $" + str(my_bet) + "."
+			]
+		player_cash += my_bet
+		suspicion += my_bet
+	
+	cash_label.text = "You have $" + str(player_cash) + "."
+	say_random(remarks)
 	make_call()
 
 
